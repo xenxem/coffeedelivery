456f06ca768e9a008cf2a72040e64131e6c97332
Carlos Alexandre Pereira <carlosa.pereira@rio.rj.gov.br>
2024-12-06 12:00:26 -0300

    formatação do campo number

--- src/components/InputContainer.styles.ts
+++ src/components/InputContainer.styles.ts
@@ -39,4 +39,39 @@ export const InputContainerSComp = styled.div`
         flex-grow: 1;
 
     }
+
+    input[type="number"]{
+
+/* Input */
+box-sizing: border-box;
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 12px;
+gap: 4px;
+
+/* Base/Input */
+background: #EEEDED;
+
+/* Base/Button */
+border: 1px solid #E6E5E5;
+border-radius: 4px;
+
+/* Text/Regular S */
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+
+/* Base/Label */
+color: #8D8686;
+
+/* Inside auto layout */
+flex: 1;
+order: 0;
+flex-grow: 1;
+
+}
 `;
